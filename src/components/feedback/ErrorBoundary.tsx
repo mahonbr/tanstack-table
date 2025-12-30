ba8fca525d12ce63bd204607e5757b0a46a4d2f0
Brent Mahoney <brent.mahoney@elevancehealth.com>
2025-12-30 16:32:10 -0500

    Convert ErrorBoundary to TypeScript

--- src/components/feedback/ErrorBoundary.tsx
+++ src/components/feedback/ErrorBoundary.tsx
@@ -1,7 +1,16 @@
-import { Component } from 'react';
+import { Component, ErrorInfo } from 'react';
 
-class ErrorBoundary extends Component {
-	constructor(props) {
+interface ErrorBoundaryProps {
+	children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+	error: Error | null;
+	errorInfo: ErrorInfo | null;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+	constructor(props: ErrorBoundaryProps) {
 		super(props);
 		this.state = { error: null, errorInfo: null };
 	}
